77182ec22dd2d9081a1bc412995be1349888bf61
Rob Anderson <rob@andrsn.uk>
2023-12-07 17:57:05 +0000

    sketching

--- assets/js/cups-graph.mjs
+++ assets/js/cups-graph.mjs
@@ -1,7 +1,7 @@
-import { Application, Sprite, Texture } from './pixi.mjs'
+import { Application, Sprite, Texture, Graphics } from './pixi.mjs'
 
 const WIDTH = 640
-const HEIGHT = 360
+const HEIGHT = 300
 
 export class Orbital {
   /**
@@ -9,25 +9,32 @@ export class Orbital {
     @param {number} xOffset
     @param {number} yOffset
     @param {number} tOffest
+    @param {number} r
   */
-  constructor(sprite, xOffset, yOffset, tOffest) {
+  constructor(sprite, xOffset, yOffset, tOffest, r) {
     this.sprite = sprite
     this.xOffset = xOffset
     this.yOffset = yOffset
     this.tOffest = tOffest
+    this.r = r
   }
   /** @param {number} timeElapsed */
   update(timeElapsed) {
     const t = timeElapsed * 0.0005 + this.tOffest * Math.PI
 
     this.sprite.zIndex = Math.cos(t) > 0 ? 1 : -1
-    this.sprite.position.x = WIDTH * 0.5 + this.xOffset + Math.sin(t) * 250
-    this.sprite.position.y = HEIGHT * 0.5 + this.yOffset + Math.cos(t) * 50
+    this.sprite.position.x = WIDTH * 0.5 + this.xOffset + Math.sin(t) * this.r
+    this.sprite.position.y =
+      HEIGHT * 0.5 + this.yOffset + Math.cos(t) * this.r * 0.3333
   }
 }
 
 export function random(min, max) {
   return min + Math.random() * Math.abs(max - min)
+}
+
+function pause(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 export class CupsGraph extends HTMLElement {
@@ -36,6 +43,8 @@ export class CupsGraph extends HTMLElement {
     height: HEIGHT,
     backgroundAlpha: 0,
   })
+  imageMask = new Graphics()
+  colourMask = new Graphics()
 
   /** @type {Orbital[]} */ orbitals = []
 
@@ -59,22 +68,44 @@ export class CupsGraph extends HTMLElement {
 
     // TODO: work out how to make the headshot a circle
 
+    const color = getComputedStyle(document.body).getPropertyValue('--color')
+    this.colourMask.beginFill(color)
+    this.colourMask.drawRect(0, 0, WIDTH, HEIGHT)
+
+    this.imageMask.beginFill(0x000000)
+    this.imageMask.drawEllipse(WIDTH / 2, HEIGHT / 2, 100, 100)
+
     const image = Sprite.from(imageSrc)
     image.position.x = WIDTH * 0.5
     image.position.y = HEIGHT * 0.5
     image.anchor.x = 0.5
     image.anchor.y = 0.5
-    image.width = 256
-    image.height = 256
+    image.width = 200
+    image.height = 200
     image.zIndex = 0
+    image.mask = this.imageMask
     this.app.stage.addChild(image)
 
     this.appendChild(this.app.view)
 
-    for (let i = 0; i < cups; i++) this.addCup()
-    for (let i = 0; i < bags; i++) this.addBean()
+    // for (let i = 0; i < cups; i++) this.addCup()
+    // for (let i = 0; i < bags; i++) this.addBean()
 
     this.app.ticker.add((dt) => this.tick(dt))
+
+    this.addOrbits(cups, bags)
+  }
+
+  async addOrbits(cups, bags) {
+    for (let i = 0; i < bags; i++) {
+      this.addBean()
+      await pause(1000 / (bags || 1))
+    }
+    await pause(1000)
+    for (let i = 0; i < cups; i++) {
+      this.addCup()
+      await pause(2000 / (cups || 1))
+    }
   }
 
   addCup() {
@@ -85,7 +116,7 @@ export class CupsGraph extends HTMLElement {
     sprite.anchor.y = 0.5
     this.app.stage.addChild(sprite)
     this.orbitals.push(
-      new Orbital(sprite, random(-50, 50), random(-50, 50), random(-1, 1)),
+      new Orbital(sprite, random(-40, 40), random(-40, 40), random(-1, 1), 180),
     )
   }
   addBean() {
@@ -96,18 +127,7 @@ export class CupsGraph extends HTMLElement {
     sprite.anchor.y = 0.5
     this.app.stage.addChild(sprite)
     this.orbitals.push(
-      new Orbital(sprite, random(-50, 50), random(-50, 50), random(-1, 1)),
-    )
-  }
-  addOrbital(imageUrl, width, height) {
-    const sprite = Sprite.from(imageUrl)
-    sprite.width = width
-    sprite.height = height
-    sprite.anchor.x = 0.5
-    sprite.anchor.y = 0.5
-    this.app.stage.addChild(sprite)
-    this.orbitals.push(
-      new Orbital(sprite, random(-50, 50), random(-50, 50), random(-1, 1)),
+      new Orbital(sprite, random(-20, 20), random(-20, 20), random(-1, 1), 280),
     )
   }
 
